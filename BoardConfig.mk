fafda10f057793d8829a2ed252a0ca5cba1b54f3
Gannon Macdonald <gannon050197@gmail.com>
2012-10-22 01:26:00 -0500

    BoardConfig: Setting VSYNC to false Signed-off-by: Gannon Macdonald <gannon050197@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_NO_HW_VSYNC := true
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
